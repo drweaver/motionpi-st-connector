13760735830a3dec6140c966d07da3414afd33cc
Shane <sgtw_speed@yahoo.com>
2020-03-07 18:05:09 +0000

    add script for motion detection

---
+++ motionpi/motion_sensor.py
@@ -0,0 +1,55 @@
+from gpiozero import MotionSensor
+import time
+
+last_motion = None
+
+pir = MotionSensor(4)
+
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+  print("rc: " + str(rc))
+
+def on_publish(client, obj, mid):
+  print("mid: " + str(mid))
+
+mqttc = mqtt.Client()
+# Assign event callbacks
+#mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+#mqttc.on_subscribe = on_subscribe
+
+mqttc.will_set("home/kitchen/motion", "inactive")
+
+mqttc.connect("192.168.0.28")
+mqttc.loop_start()
+
+
+def when_motion():
+  global last_motion
+  if last_motion == None: mqttc.publish("home/kitchen/motion", "active")
+  last_motion = time.time()
+  print("last_motion = " + str(last_motion))
+
+def when_no_motion():
+  mqttc.publish("home/kitchen/motion", "inactive")
+
+pir.when_motion = when_motion
+#pir.when_no_motion = when_no_motion
+
+when_no_motion()
+
+while True:
+  if last_motion != None and time.time() - last_motion > 30:
+    when_no_motion()
+    last_motion = None;
+  time.sleep(1)
+
+#while True:
+#  pir.wait_for_motion()
+#  localtime = time.localtime()
+#  result = time.strftime("%I:%M:%S %p", localtime)
+#  print("Motion Detected at " + result)
+#  mqttc.publish("home/kitchen/motion", str(int(time.time())))
+#  time.sleep(1)
